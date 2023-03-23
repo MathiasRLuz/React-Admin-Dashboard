188f4a0e433d840f0bd1981f87f94e568efaf53e
Mathias R Luz <mathias_luz@outlook.com>
2023-03-23 13:05:27 -0300

    Pie Chart with Nivo Charts

---
+++ react-admin-dashboard/src/scenes/pie/index.js
@@ -0,0 +1,15 @@
+import { PieChart } from "../../components/PieChart";
+import { Header } from "../../components/Header";
+import { Box } from "@mui/material";
+
+export const Pie = () => {
+  return (
+    <Box m="20px">
+      <Header title="Pie Chart" subtitle="Simple Pie Chart" />
+
+      <Box height="75vh">
+        <PieChart />
+      </Box>
+    </Box>
+  );
+};
