6dceb0f16549f9fa55c2014733de040d788f4687
Mathias R Luz <mathias_luz@outlook.com>
2023-03-23 12:50:32 -0300

    Bar Chart

---
+++ react-admin-dashboard/src/scenes/bar/index.jsx
@@ -0,0 +1,15 @@
+import { BarChart } from "../../components/BarChart";
+import { Header } from "../../components/Header";
+import { Box } from "@mui/material";
+
+export const Bar = () => {
+  return (
+    <Box m="20px">
+      <Header title="Bar Chart" subtitle="Simple Bar Chart" />
+
+      <Box height="75vh">
+        <BarChart />
+      </Box>
+    </Box>
+  );
+};
