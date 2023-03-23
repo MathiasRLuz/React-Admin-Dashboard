c511dd0c9f850af7c30252ca91a0400d4ecd7bc7
Mathias R Luz <mathias_luz@outlook.com>
2023-03-23 10:28:26 -0300

    Profile Form with Formik and Yup

--- react-admin-dashboard/src/App.js
+++ react-admin-dashboard/src/App.js
@@ -5,15 +5,15 @@ import { Topbar } from "./scenes/global/Topbar";
 import { MySidebar } from "./scenes/global/MySidebar";
 import { Dashboard } from "./scenes/dashboard";
 import { Team } from "./scenes/team";
-import { Invoices } from "./scenes/invoices";
 import { Contacts } from "./scenes/contacts";
-// import { Bar } from "./scenes/bar";
-// import { Form } from "./scenes/form";
-// import { Line } from "./scenes/line";
-// import { Pie } from "./scenes/pie";
-// import { FAQ } from "./scenes/faq";
-// import { Geography } from "./scenes/geography";
+import { Invoices } from "./scenes/invoices";
+import { Form } from "./scenes/form";
 // import { Calendar } from "./scenes/calendar";
+// import { FAQ } from "./scenes/faq";
+// import { Bar } from "./scenes/bar";
+// import { Pie } from "./scenes/pie";
+// import { Line } from "./scenes/line";
+// import { Geography } from "./scenes/geography";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -29,15 +29,15 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
-              <Route path="/invoices" element={<Invoices />} />
               <Route path="/contacts" element={<Contacts />} />
-              {/* <Route path="/bar" element={<Bar />} /> */}
-              {/* <Route path="/form" element={<Form />} /> */}
-              {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/pie" element={<Pie />} /> */}
-              {/* <Route path="/faq" element={<FAQ />} /> */}
-              {/* <Route path="/geography" element={<Geography />} /> */}
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
               {/* <Route path="/calendar" element={<Calendar />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
         </div>
